656b1c634214232fab024e7717562f4a3bfb1ab8
Singh-csm <csm8107@gmail.com>
2023-06-08 23:17:25 +0530

    mongo: cluster

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div style={{display:"flex", justifyContent:"space-between"}}>
+
+      <p style={{border:"3px solid green", borderRadius:"15px",padding:"14px"}}>mongodb+srv://singh-csm:GIUPM5681K@singh-csm.nmfw5jk.mongodb.net/test</p>
+    </div>
+  )
+}
+
+export default App
